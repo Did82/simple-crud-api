2c0aa1287cd5287288e71c5fc0e1840bf56dcbd5
Dima <dikashko@gmail.com>
2021-11-26 10:42:18 +0300

    feat: starting and setting project

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+};
+
+module.exports = config;
